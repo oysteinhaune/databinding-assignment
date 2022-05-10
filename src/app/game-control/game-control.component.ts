46f29045f342cc5574b77c73964488c7672e8355
Øystein <oysteinha@gmail.com>
2022-05-10 15:47:36 +0200

    Components, counter
    Added components and counter, general setup.

---
+++ src/app/game-control/game-control.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit {
+  @Input() counter: { type: number, name: string, value: number };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  
+}

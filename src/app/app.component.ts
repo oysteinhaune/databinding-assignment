e4b924c0b205f84ce821fe06ed9748b46ba8d615
Øystein <oysteinha@gmail.com>
2022-05-11 13:33:33 +0200

    Counter, odd even
    interval counter emitter, check odd and even numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,23 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  counter: number = 0;
+  numbers = [];
+
+  onCounterCreated(Counterdata: {value: number}) {
+    console.log('Counter: ' + Counterdata.value)
+  }
+
+  oddNumberCreated(oddNumberData: {value: number}) {
+    this.numbers.push({
+      value: oddNumberData.value,
+      type: 'Odd - NUMBER'
+    })
+  }
+
+  evenNumberCreated(evenNumberData: {value: number}) {
+    this.numbers.push({
+      value: evenNumberData.value,
+      type: 'Even - NUMBER'
+    })
+  }
 }
--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,12 +6,38 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
-  @Input() counter: { type: number, value: number };
-
+  IntervalCounter;
+  IntervalCounterIndex = 0;
+  
+  @Output() counterCreated = new EventEmitter<{ value: number }>();
+  @Output() oddNumberCreated = new EventEmitter<{ value: number}>();
+  @Output() evenNumberCreated = new EventEmitter<{ value: number}>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  
+  startIntervalCounter() {
+    this.IntervalCounter = setInterval(() => {
+      this.IntervalCounterIndex += 1
+      this.counterCreated.emit({ value: this.IntervalCounterIndex});
+      this.numberCheck();
+    }, 1000)
+  }
+
+  numberCheck() {
+    if(this.IntervalCounterIndex % 2 === 0) {
+      this.oddNumberCreated.emit({ value: this.IntervalCounterIndex});
+    } else {
+      this.evenNumberCreated.emit({ value: this.IntervalCounterIndex});
+    }
+  }
+
+  onGameStart() {
+    this.startIntervalCounter()
+  }
+
+  onGameStop() {
+    clearInterval(this.IntervalCounter)
+  }
 }
